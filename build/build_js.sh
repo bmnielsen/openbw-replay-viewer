9799f2410458355d34df1fa74c72f3f956dd1d80
Bruce Nielsen <bmnielsen@users.noreply.github.com>
2024-12-08 08:22:27 +0100

    Make build script more readable

--- build/build_js.sh
+++ build/build_js.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-# em++ -std=c++14 \
-#     -I openbw/ -I sdl/ \
-#     -ferror-limit=2 -O3 --bind -D USE_DL_PREFIX -DMSPACES -DFOOTERS -g1
-#     -s ASM_JS=1 -s USE_SDL=2 -s TOTAL_MEMORY=201326592 -s INVOKE_RUN=0 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS="['png']" -s USE_SDL_MIXER=2 -s DISABLE_EXCEPTION_CATCHING=1 -s ASSERTIONS=1 -s ABORTING_MALLOC=0 \
-#     -s EXPORTED_FUNCTIONS="['_main','_ui_resize','_replay_get_value','_replay_set_value','_player_get_value','_load_replay']" \
-#     -o openbw.html \
-#     openbw/ui/sdl2.cpp openbw/ui/gfxtest.cpp dlmalloc/malloc.c
-
-em++ -std=c++14 -I openbw/ openbw/ui/sdl2.cpp openbw/ui/gfxtest.cpp -ferror-limit=2 -O3  -s ASM_JS=1 -s USE_SDL=2 -o openbw.html -s TOTAL_MEMORY=201326592 -s INVOKE_RUN=0 -s EXPORTED_FUNCTIONS="['_main','_ui_resize','_replay_get_value','_replay_set_value','_player_get_value','_load_replay']" --bind -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS="['png']" -s USE_SDL_MIXER=2 -s DISABLE_EXCEPTION_CATCHING=1 -D USE_DL_PREFIX -I sdl/ dlmalloc/malloc.c -s ABORTING_MALLOC=0 -DMSPACES -DFOOTERS -g1 -s ASSERTIONS=1
+em++ -std=c++14 \
+    -I openbw/ -I sdl/ \
+    -ferror-limit=2 -O3 --bind -D USE_DL_PREFIX -DMSPACES -DFOOTERS -g1 \
+    -s ASM_JS=1 -s USE_SDL=2 -s TOTAL_MEMORY=201326592 -s INVOKE_RUN=0 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS="['png']" -s USE_SDL_MIXER=2 -s DISABLE_EXCEPTION_CATCHING=1 -s ASSERTIONS=1 -s ABORTING_MALLOC=0 \
+    -s EXPORTED_FUNCTIONS="['_main','_ui_resize','_replay_get_value','_replay_set_value','_player_get_value','_load_replay']" \
+    -o openbw.html \
+    openbw/ui/sdl2.cpp openbw/ui/gfxtest.cpp dlmalloc/malloc.c
